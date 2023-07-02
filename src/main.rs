f80b4786905c3c159e30129a0555fa670c5a206b
Jibran Kalia <jibran.kalia@gmail.com>
2023-07-02 16:36:45 -0500

    feat: print arguments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use clap::{Parser, command};
+
+#[derive(Parser)]
+#[command(version)]
+#[command(about = "Does awesome things", long_about = None)]
+struct Cli {
+    /// The pattern to look for
+    #[arg(long)]
+    pattern: String,
+
+    /// The path to the file to read
+    #[arg(long)]
+    path: std::path::PathBuf,
+} 
+
+
 fn main() {
-    println!("Hello, world!");
+  let args = Cli::parse();
+  print!("Pattern: {}, Path: {}", args.pattern, args.path.display());
+
 }
