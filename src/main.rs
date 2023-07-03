210b5c59c128fec7a2a07fac9867e60f47e0d76f
Jibran Kalia <jibran.kalia@gmail.com>
2023-07-03 10:03:21 -0500

    feat: accepting paths as a param

--- src/main.rs
+++ src/main.rs
@@ -4,15 +4,21 @@ use serde_json::Result;
 use std::collections::HashMap;
 use std::fs;
 use std::path::Path;
+use std::path::PathBuf;
 use walkdir;
-use walkdir::WalkDir;
 
 #[derive(Parser)]
 #[command(version)]
 #[command(about = "Does awesome things", long_about = None)]
 struct Cli {
     #[arg(long, short)]
-    path: std::path::PathBuf,
+    base: std::path::PathBuf,
+
+    #[arg(long, short, num_args = 1..)]
+    paths: Vec<std::path::PathBuf>,
+
+    #[arg(long, short)]
+    test: bool,
 }
 
 #[derive(Debug, Deserialize)]
@@ -46,67 +52,69 @@ fn compare_dependencies(
 ) -> Option<Mismatch> {
     for (key, value) in current {
         match root.get(key) {
-            Some(val) => if val != value {
+            Some(val) => {
+                if val != value {
                     return Some(Mismatch {
                         key: key.clone(),
                         root_value: val.clone(),
                         current_value: value.clone(),
                     });
-                },
-            None => ()
+                }
+            }
+            None => (),
         }
     }
     None
 }
 
-fn find_child_package_json(path: &Path, root: &PackageDependencies) -> Option<String> {
-    let root_dir = path.parent().unwrap();
-    for entry in WalkDir::new(root_dir) {
-        let entry = entry.unwrap();
-        // ensure package.json is not in node_modules
-        return if !entry.file_type().is_file() && entry.file_name().to_str() == Some("node_modules") {
-            None
-        } else
-        if entry.file_type().is_file() && entry.file_name().to_str() == Some("package.json") && entry.file_name().to_str() != Some("node_modules") {
-            println!("Checking file {}", entry.path().display());
-            let current_deps = read_file(entry.path()).unwrap();
-            match compare_dependencies(&root, &current_deps) {
-                Some(mismatch) => {
-                    let result = format!(
-                        "Expected: {} but found: {} in {}",
-                        mismatch.root_value, mismatch.current_value, mismatch.key
-                    );
-                    return Some(result);
-                }
-                None => (),
-            }
+fn find_child_package_json(path: &Path, root_details: &PackageDependencies) -> Option<String> {
+    let current_deps = read_file(path)?;
+    match compare_dependencies(&root_details, &current_deps) {
+        Some(mismatch) => {
+            let result = format!(
+                "Expected: {} but found: {} in {}",
+                mismatch.root_value, mismatch.current_value, mismatch.key
+            );
+            return Some(result);
         }
+        None => None,
     }
-    None
 }
 
 fn main() {
     let args = Cli::parse();
 
-    let root_package_json = args.path.clone();
-    let path = Path::new(&root_package_json);
+    let base_path = args.base;
 
-    let root_deps = match read_file(path) {
+    let root_package: &std::path::PathBuf = &base_path.join("package.json");
+    let root_package_path = root_package.as_path();
+
+    let to_check: Vec<PathBuf> = args
+        .paths
+        .iter()
+        .map(|path_buf| base_path.join(path_buf).clone())
+        .collect();
+
+    println!("Here are the paths to check: {:?}", to_check);
+
+    let root_deps = match read_file(root_package_path) {
         Some(deps) => deps,
         None => {
             eprintln!(
                 "Failed to parse the root package.json on this path: {}",
-                path.display()
+                root_package_path.display()
             );
             std::process::exit(1);
         }
     };
 
-    match find_child_package_json(path, &root_deps) {
-        Some(message) => {
-            println!("Mismatch: {}", message);
-            std::process::exit(1);
+    for path in to_check {
+        match find_child_package_json(path.as_path(), &root_deps) {
+            Some(message) => {
+                println!("Mismatch: {}", message);
+                std::process::exit(1);
+            }
+            None => println!("No mismatch in {}", path.display()),
         }
-        None => println!("No mismatch"),
     }
 }
